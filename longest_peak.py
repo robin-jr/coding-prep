d61688c7a61a9cca9ef22739f5f5a3c33b2ff601
Robin J <robin__j@outlook.com>
2022-02-27 23:20:29 +0530

    updated longes_peak solution

--- longest_peak.py
+++ longest_peak.py
@@ -1,39 +1,28 @@
 def longestPeak(array):
     n=len(array)
-    length=0
-    direction=0
     maxLength=0
-    for i in range(1,n):
-        r= array[i]
-        l = array[i-1]
-        if r==l: #reset direction
-            if length>=maxLength and length!=0 and direction==-1:
-                maxLength=length+1
-            direction=0
-            length=0
-        elif r>l and direction!=1: #reset direction
-            if length>maxLength and length!=0:
-                maxLength=length+1 #including the current position 
-            direction=1
-            length=2
-        elif r<l and direction!=-1: #reset direction
-            if length>maxLength and length!=0:
-                maxLength=length+1
-            if direction!=1:
-                continue
-            direction=-1
-        elif r>l and direction==1: #increase length
-            length+=1
-        elif r<l and direction ==-1: #increase length
-            length+=1
-        if length+1>maxLength and direction==-1:
-            maxLength=length+1 #last element
+    i=1
+    while i<n-1:
+       isPeak=array[i]>array[i-1] and array[i]>array[i+1]
+       if not isPeak:
+           i+=1    
+           continue
+       leftIndex=i-2
+       while leftIndex>=0 and array[leftIndex]<array[leftIndex+1]:
+           leftIndex-=1
+       rightIndex=i+2
+       while rightIndex<n and array[rightIndex]<array[rightIndex-1]:
+           rightIndex+=1
+       currentLength=rightIndex-leftIndex-1
+       maxLength=max(currentLength,maxLength)
+       i=rightIndex
     return maxLength
+
 array=[1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
-# array=[1, 2, 3,1,2,3,4,5,4]
+array=[1, 2, 3,1,2,3,4,5,4]
 # array=[5, 4, 3, 2, 1, 2, 1]
 # array= [1, 1, 3, 2, 1]
-array =[1, 2, 3, 2, 1, 1]
+# array =[1, 2, 3, 2, 1, 1]
 x=longestPeak(array)
 
 print(x)
