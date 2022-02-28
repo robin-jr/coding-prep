cdf43cb32e399caa48d658d18f110b7f38f17c39
Robin J <robin__j@outlook.com>
2022-02-28 11:24:48 +0530

    updated array_of_products

--- array_of_products.py
+++ array_of_products.py
@@ -1,19 +1,20 @@
 def arrayOfProducts(array):
     n=len(array)
-    leftProducts=[1 for _ in range(n)]
-    rightProducts=[1 for _ in range(n)]
     products=[1 for _ in range(n)]
     
-    for i in range(1,n):
-        leftProducts[i] =leftProducts[i-1]*array[i-1]
-        products[i] = leftProducts[i]
-    
-    for i in range(n-2,-1,-1):
-        rightProducts[i] =rightProducts[i+1]*array[i+1]
-        products[i]*=rightProducts[i]
+    leftRunningProduct=1
+    for i in range(n):
+        products[i] = leftRunningProduct
+        leftRunningProduct*=array[i]
+        
+    rightRunningProduct=1
+    for i in range(n-1,-1,-1):
+        products[i]*=rightRunningProduct
+        rightRunningProduct*=array[i]
     
     return products
     
 
 array=[5, 1, 4, 2]
 x=arrayOfProducts(array)
+print(x)
