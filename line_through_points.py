2b7b8d87560709e75f9c17b78d5ac28f0e34ce11
Robin J <robin__j@outlook.com>
2022-03-01 14:34:06 +0530

    line_through_points done

---
+++ line_through_points.py
@@ -0,0 +1,35 @@
+def calculateSlope(point1,point2):
+    try:
+        return (point2[1]-point1[1]) / (point2[0]-point1[0])
+    except:
+        return None
+
+def lineThroughPoints(points):
+    n=len(points)
+    maxCount=1
+    if n==1:
+        return maxCount
+    for i in range(n):
+        dict={}
+        for j in range(n):
+            if i==j:
+                continue
+            currentSlope=calculateSlope(points[i],points[j])
+            dict[currentSlope]=dict.get(currentSlope,0)+1
+        c= max(dict.values())
+        maxCount = max(c+1,maxCount)
+    return maxCount
+
+
+points= [
+    [1, 1],
+    [2, 2],
+    [3, 3],
+    [0, 4],
+    [-2, 6],
+    [4, 0],
+    [2, 1]
+  ]
+
+x = lineThroughPoints(points)
+print(x)
