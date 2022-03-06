f1cf6a072c9d670113fcf0ce91165f154404c22e
Robin J <robin__j@outlook.com>
2022-03-06 11:13:18 +0530

    bug fix

--- minimum_number_of_coins_for_change.py
+++ minimum_number_of_coins_for_change.py
@@ -4,5 +4,5 @@ def minNumberOfCoinsForChange(n, denoms):
     for denom in denoms:
         for amount in range(1,n+1):
             if denom <= amount:
-                minCoins[amount] = min(minCoins[amount], minCoins[denom-amount]+1)
+                minCoins[amount] = min(minCoins[amount], minCoins[amount-denom]+1)
     return minCoins[-1] if minCoins[-1] != float("inf") else -1
