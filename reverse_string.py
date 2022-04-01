40efaa7533d1860cef12d27bfe6492af730ea731
Robin J <robin__j@outlook.com>
2022-04-01 14:22:06 +0530

    string reverse done

---
+++ reverse_string.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        n = len(s)
+        for i in range(n//2):
+            s[i],s[n-i-1] = s[n-i-1],s[i]
+        return s
+    
+
+a=['a','b','c']
+s = Solution().reverseString(a)
+print(a,s)
