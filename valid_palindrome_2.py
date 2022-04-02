c0c512bdd5dfdf38f23e4d22802fc4675e536ef3
Robin J <robin__j@outlook.com>
2022-04-02 15:17:07 +0530

    valid_palindrome_2 done

---
+++ valid_palindrome_2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def validPalindrome(self, s: str, deletion_count=0) -> bool:
+        n=len(s)
+        if n<2: return deletion_count<=1
+        if deletion_count>1: return False
+        while s[0]==s[-1] and n>2:
+            s=s[1:n-1]
+            n-=2
+        if s[0]==s[-1]: return self.validPalindrome(s[1:n-1])
+        if self.validPalindrome(s[1:n], deletion_count+1) or self.validPalindrome(s[0:n-1], deletion_count+1): return True
+        return False
+    
+s = "cupuupucua"
+print(Solution().validPalindrome(s))
+ 
