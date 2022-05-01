8d51b96cac1108bbd758f9cd359e61efed7224df
Robin J <robin__j@outlook.com>
2022-05-02 02:41:42 +0530

    phone_number_pnemonics upgraded

--- phone_number_pnemonics.py
+++ phone_number_pnemonics.py
@@ -1,17 +1,20 @@
 pad = {
-    '1':['1'],
-    '2':['a', 'b', 'c'],
-    '3':['d', 'e', 'f'],
-    '4':['g', 'h', 'i'],
-    '5':['j', 'k', 'l'],
-    '6':['m', 'n', 'o'],
-    '7':['p', 'q', 'r', 's'],
-    '8':['t', 'u', 'v'],
-    '9':['w', 'x', 'y', 'z'],
-    '0':['0']
+    '1': ['1'],
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z'],
+    '0': ['0']
 }
+
+
 def phoneNumberMnemonics(phoneNumber):
-    mnemonics =['']
+    # return helper(phoneNumber, ['']*len(phoneNumber))
+    mnemonics = ['']
     for e in phoneNumber:
         l = len(mnemonics)
         newMnemonics = []
@@ -21,6 +24,18 @@ def phoneNumberMnemonics(phoneNumber):
                 newMnemonics.append(c+f)
         mnemonics = newMnemonics
     return mnemonics
+
+
+def helper(phoneNumber, current, idx=0):
+    result = []
+    if idx == len(phoneNumber):
+        result.append("".join(current))
+        return result
+    for e in pad.get(phoneNumber[idx]):
+        current[idx] = e
+        result += helper(phoneNumber, current, idx+1)
+    return result
+
 
 x = phoneNumberMnemonics("1905")
 print(x)
