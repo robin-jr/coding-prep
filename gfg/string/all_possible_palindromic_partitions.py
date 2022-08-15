5ecf3529fa92a3732a6aa7ab59ad1a22b1d1fe02
Robin J <robin__j@outlook.com>
2022-08-15 22:36:44 +0530

    all_possible_palindromic_partitions updated

--- gfg/string/all_possible_palindromic_partitions.py
+++ gfg/string/all_possible_palindromic_partitions.py
@@ -11,8 +11,26 @@ def palindromic_partitions(s: str, all, current, start: int):
             current.append(t)
             palindromic_partitions(s, all, current, i+1)
             current.pop()
+            
+def palindromic_partitions1(arr: list[str]):
+    res = set()
+    res.add(tuple(arr))
+    if len(arr)<=1: return res
+    
+    for i in range(1, len(arr)):
+        if arr[i-1]==arr[i][::-1]: 
+            t = arr[:i-1] + [arr[i-1]+ arr[i]]+arr[i+1:]
+            res.update(palindromic_partitions1(t))
+        elif i+1<len(arr) and arr[i-1]==arr[i+1][::-1]:
+            t = arr[:i-1]+[arr[i-1]+arr[i]+arr[i+1]]+arr[i+2:]
+            res.update(palindromic_partitions1(t))
+    return res
 
+s = "nitin"
 all = []
-palindromic_partitions("nitin", all, [], 0)
+palindromic_partitions(s, all, [], 0)
 print(all)
+
+x = palindromic_partitions1(list(s))
+print(list(x))
 
