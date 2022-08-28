19154f87c9391e2cb91ff96367a960211c56d546
Robin J <robin__j@outlook.com>
2022-08-28 23:39:01 +0530

    validate_three_nodes updated

--- hard/validate_three_nodes.go
+++ hard/validate_three_nodes.go
@@ -29,3 +29,23 @@ func ValidateThreeNodes(nodeOne *BST, nodeTwo *BST, nodeThree *BST) bool {
 	}
 	return false
 }
+
+func oneHasThree(root *BST, mid *BST, last *BST, isMidFound bool) bool {
+	if root == nil {
+		return false
+	}
+	if root.Value == mid.Value {
+		isMidFound = true
+	}
+	if root.Value == last.Value {
+		return isMidFound
+	}
+	if root.Value < last.Value {
+		return oneHasThree(root.Right, mid, last, isMidFound)
+	}
+	return oneHasThree(root.Left, mid, last, isMidFound)
+}
+
+func ValidateThreeNodes1(nodeOne *BST, nodeTwo *BST, nodeThree *BST) bool {
+	return oneHasThree(nodeOne, nodeTwo, nodeThree, false) || oneHasThree(nodeThree, nodeTwo, nodeOne, false)
+}
