871e9f732ec4763b31f97cfcc0ad35a0823a03be
Robin J <robin__j@outlook.com>
2022-09-05 23:32:17 +0530

    max_sum_increasing_subsequence updated

--- hard/max_sum_increasing_subsequence.go
+++ hard/max_sum_increasing_subsequence.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 var maxSum int
 var maxes []int
@@ -26,4 +29,51 @@ func alg(array []int, start int, sumFar int, res []int) {
 		maxSum = sumFar
 		maxes = res
 	}
+}
+
+type E struct {
+	value, previousI int
+}
+
+func (e E) String() string {
+	return fmt.Sprintf("%v %v\n", e.value, e.previousI)
+}
+func MaxSumIncreasingSubsequence1(array []int) (int, []int) {
+	maxesSoFar := []E{{array[0], -1}}
+	maxSumIndex := 0
+	for i := 1; i < len(array); i += 1 {
+		curr_max := E{array[i], i}
+		for j := i - 1; j >= 0; j -= 1 {
+			if array[j] < array[i] && array[i]+maxesSoFar[j].value > curr_max.value {
+				curr_max = E{array[i] + maxesSoFar[j].value, j}
+			}
+		}
+		maxesSoFar = append(maxesSoFar, curr_max)
+
+		if maxesSoFar[i].value > maxesSoFar[maxSumIndex].value {
+			maxSumIndex = i
+		}
+	}
+
+	res := []int{array[maxSumIndex]}
+	for i := maxSumIndex; i != -1 && i != maxesSoFar[i].previousI; {
+		i = maxesSoFar[i].previousI
+		if i == -1 {
+			break
+		}
+		res = append(res, array[i])
+	}
+
+	// reversing
+	for i, j := 0, len(res)-1; i < j; i, j = i+1, j-1 {
+		res[i], res[j] = res[j], res[i]
+	}
+
+	return maxesSoFar[maxSumIndex].value, res
+}
+
+func main() {
+	array := []int{10, 70, 20, 30, 50, 11, 30, 5}
+	x, y := MaxSumIncreasingSubsequence1(array)
+	fmt.Println(x, y)
 }
