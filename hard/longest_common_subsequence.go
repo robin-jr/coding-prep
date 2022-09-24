88d02c178e6bcf3cf95d0a810ba2fe4471d1b211
Robin J <robin__j@outlook.com>
2022-09-24 07:05:41 +0530

    longest common subsequence updated

--- hard/longest_common_subsequence.go
+++ hard/longest_common_subsequence.go
@@ -23,11 +23,41 @@ func h(s1, s2 []rune, s string, i, j int) {
 }
 
 // O(nm*min(n, m)) time | O(nm*min(n, m)) space
-func LongestCommonSubsequence(s1 string, s2 string) []byte {
+func LongestCommonSubsequence0(s1 string, s2 string) []byte {
 	// Write your code here.
 	h([]rune(s1), []rune(s2), "", 0, 0)
 	fmt.Println(longestSS)
 	return []byte(longestSS)
+}
+
+func getMaxLengthString(s1 string, s2 string) string {
+	if len(s1) == len(s2) {
+		return s1
+	}
+	if len(s1) > len(s2) {
+		return s1
+	}
+	return s2
+}
+
+func LongestCommonSubsequence(s1 string, s2 string) []byte {
+	d := make([][]string, len(s1)+1)
+
+	for i := range d {
+		d[i] = make([]string, len(s2)+1)
+	}
+
+	for i := 1; i < len(s1)+1; i++ {
+		for j := 1; j < len(s2)+1; j++ {
+			if s1[i-1] == s2[j-1] {
+				d[i][j] = d[i-1][j-1] + string(s1[i-1])
+			} else {
+				d[i][j] = getMaxLengthString(d[i-1][j], d[i][j-1])
+			}
+		}
+	}
+
+	return []byte(d[len(s1)][len(s2)])
 }
 
 func main() {
