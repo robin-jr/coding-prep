f98e7ede098055eabdc2030bdf35bd5ac9b41368
Robin J <robin__j@outlook.com>
2022-09-24 19:49:51 +0530

    min jumps updated

--- hard/min_jumps.go
+++ hard/min_jumps.go
@@ -9,7 +9,7 @@ type e struct {
 	index int
 }
 
-func MinNumberOfJumps(array []int) int {
+func MinNumberOfJumps0(array []int) int {
 	count := 0
 	for i := 0; i < len(array)-1; {
 		h := e{0, 0}
@@ -32,10 +32,33 @@ func MinNumberOfJumps(array []int) int {
 	return count
 }
 
+func MinNumberOfJumps(array []int) int {
+	jumps := 0
+	maxReach := array[0]
+	steps := maxReach
+	for i := 1; i < len(array); i++ {
+		if i == len(array)-1 {
+			return jumps + 1
+		}
+		maxReach = max(maxReach, array[i]+i)
+		steps -= 1
+		if steps == 0 {
+			steps = maxReach - i
+			jumps += 1
+		}
+	}
+	return jumps
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
 func main() {
-	array := []int{3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3}
+	// array := []int{3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3}
 	// array := []int{3, 12, 2, 1, 2, 3, 7, 1, 1, 1, 3, 2, 3, 2, 1, 1, 1, 1}
-	// array := []int{2, 1, 2, 3, 1}
+	array := []int{2, 1, 2, 3, 1}
 	x := MinNumberOfJumps(array)
 	fmt.Println(x)
 }
